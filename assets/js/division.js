48565304be8cc1bf4dc40b18c2b52940364d9fd4
kash-dotcom <katyashworth27@gmail.com>
2024-09-14 12:15:30 +0000

    finished the divide function

---
+++ assets/js/division.js
@@ -0,0 +1,29 @@
+
+function findFactors(num) {
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let factors = [];
+    for (let i = 0; i <=num1; i++) 
+        if (num1 % i == 0);
+}
+console.log(findFactors)
+
+let num3 =findNum[Math.floor(Math.random()* findNum.length)];
+
+console.log(num3);
+
+  //https://www.programiz.com/javascript/examples/factors-number
+        /**
+         * Get the factors of num1 
+         */
+
+        for (let i = 0; i <=num1; i++) 
+            if (num1 % i == 0);
+        
+        //https://www.geeksforgeeks.org/how-to-select-a-random-element-from-array-in-javascript/    
+        /**
+         * random selection of number
+         */
+        var numArr = i;
+        var random;
+        function num3() {
+            random = Math.floor(Math.random() * i.length);
